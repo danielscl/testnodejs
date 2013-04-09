877b76479a19ca80f47ba609825674316f1d488c
defrainfox <daniel@sclbits.com>
2013-04-09 01:01:39 -0500

    add new

---
+++ server.js
@@ -0,0 +1,11 @@
+var myserver = require('socket.io').listen(8080);
+
+myserver.sockets.on('connection',arranque);
+
+function arranque(usuario){
+	usuario.on('nuevoDato',emitir);
+}
+
+function emitir(data){
+	myserver.sockets.emit('nombreDesdeServidor',data);
+}
